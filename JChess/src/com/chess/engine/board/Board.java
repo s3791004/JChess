1d755ca58db4b87a350a736fb22f2628436c1f89
Andrew <s3791004@student.rmit.edu.au>
2020-05-17 22:44:55 +1000

    Board1-2

--- JChess/src/com/chess/engine/board/Board.java
+++ JChess/src/com/chess/engine/board/Board.java
@@ -1,5 +1,7 @@
 package com.chess.engine.board;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import com.chess.engine.Alliance;
@@ -7,11 +9,26 @@ import com.chess.engine.pieces.Piece;
 
 public class Board {
 	
+	private final List<Tile> gameBoard;
+	
 	private Board(Builder builder) {
-		
+		this.gameBoard = createGameBoard(builder);
 	}
 
 	public Tile getTile(final int tileCoordinate) {
+		return null;
+	}
+	
+	private static List<Tile> createGameBoard(final Builder builder) {
+		final Tile[] tiles = new Tile[BoardUtils.NUM_TILES];
+		for (int i = 0; i < BoardUtils.NUM_TILES; i++) {
+			tiles[i] = Tile.createTile(i, builder.boardConfig.get(i));
+		}
+		return Arrays.asList(tiles);
+	}
+	
+	public static Board createStandardBoard() {
+		//TODO video 15!!!
 		return null;
 	}
 	
